6a0171e73cc2133c4da721666f4f617566e14ebf
David <nanjiladavid2@gmail.com>
2025-03-02 01:57:00 +0300

    Fix the on and off to enhance UI
    Signed-off-by: David <nanjiladavid2@gmail.com>

--- app/src/main/java/com/example/smartspot/MainActivity.kt
+++ app/src/main/java/com/example/smartspot/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.smartspot
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Get references to UI elements
+        val toggleHotspot = findViewById<Switch>(R.id.toggle_hotspot)
+        val deviceListContainer = findViewById<LinearLayout>(R.id.device_list_container)
+
+        // Set switch listener to show/hide WiFi devices
+        toggleHotspot.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Switch is ON - Show device list
+                deviceListContainer.visibility = LinearLayout.VISIBLE
+            } else {
+                // Switch is OFF - Hide device list
+                deviceListContainer.visibility = LinearLayout.GONE
+            }
         }
     }
 }
